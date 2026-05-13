ee9dfb6b572e347754aa284c0c8afcf657acb653
nayamathboutbth <namt24@student.bth.se>
2026-05-13 16:47:46 +0200

    Fix get_user_by_email edge cases

--- backend/src/controllers/usercontroller.py
+++ backend/src/controllers/usercontroller.py
@@ -2,7 +2,7 @@ from src.controllers.controller import Controller
 from src.util.dao import DAO
 
 import re
-emailValidator = re.compile(r'.*@.*')
+emailValidator = re.compile(r'.+@.+\..+')
 
 class UserController(Controller):
     def __init__(self, dao: DAO):
@@ -30,7 +30,9 @@ class UserController(Controller):
 
         try:
             users = self.dao.find({'email': email})
-            if len(users) == 1:
+            if len(users) == 0:
+                return None
+            elif len(users) == 1:
                 return users[0]
             else:
                 print(f'Error: more than one user found with mail {email}')
