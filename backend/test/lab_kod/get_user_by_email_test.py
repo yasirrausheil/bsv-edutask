3874ec13f5dd7f783b3b6a023c8830f5e3281602
nayamathboutbth <namt24@student.bth.se>
2026-05-13 15:13:46 +0200

    Fixed lab2

--- backend/test/lab_kod/get_user_by_email_test.py
+++ backend/test/lab_kod/get_user_by_email_test.py
@@ -2,9 +2,12 @@ import pytest
 from unittest.mock import Mock 
 from src.controllers.usercontroller import UserController
 
+@pytest.fixture
+def fake_dao():
+    return Mock()
+
 #valid test case: valid email, one user exists
-def test_get_user_by_email_valid_email_one_user_exists(): 
-    fake_dao = Mock()
+def test_get_user_by_email_valid_email_one_user_exists(fake_dao): 
     user = {"email": "user@example.com", "user": "user1"}
     fake_dao.find.return_value = [user]
 
@@ -15,8 +18,7 @@ def test_get_user_by_email_valid_email_one_user_exists():
     assert result == user
 
 #edge test case: valid email, multiple users exist
-def test_get_user_by_email_valid_email_multiple_users_exist():
-    fake_dao = Mock()
+def test_get_user_by_email_valid_email_multiple_users_exist(fake_dao):
     user1 = {"email": "user@example.com", "user": "user1"}
     user2 = {"email": "user@example.com", "user": "user2"}
     fake_dao.find.return_value = [user1, user2]
@@ -29,8 +31,7 @@ def test_get_user_by_email_valid_email_multiple_users_exist():
 
 
 #invalid test case: valid email, no user exists
-def test_get_user_by_email_valid_email_no_user_exists():
-    fake_dao = Mock()
+def test_get_user_by_email_valid_email_no_user_exists(fake_dao):
     fake_dao.find.return_value = []
 
     controller = UserController(fake_dao)
@@ -40,21 +41,28 @@ def test_get_user_by_email_valid_email_no_user_exists():
     assert result is None
 
 
-#invalid test case: invalid email format
-def test_get_user_by_email_invalid_email_format():
-    fake_dao = Mock()
+#invalid test case: invalid email, missing @ symbol
+def test_get_user_by_email_invalid_email_missing_at_symbl(fake_dao):
     controller = UserController(fake_dao)
     
     with pytest.raises(ValueError):
-        controller.get_user_by_email("invalid-email")
+        controller.get_user_by_email("user.example.com")
+
+
+#invalid test case: invalid email, missing host after dot
+def test_get_user_by_email_invalid_email_missing_host_after_dot(fake_dao):
+    controller = UserController(fake_dao)
+
+    with pytest.raises(ValueError):
+        controller.get_user_by_email("user@example.")
+
 
 #invalid test case: database connection error
-def test_get_user_by_email_database_error():
-    fake_dao = Mock()
+def test_get_user_by_email_database_error(fake_dao):
     fake_dao.find.side_effect = Exception("Database connection error")
 
     controller = UserController(fake_dao)
 
-    with pytest.raises(Exception) as exc_info:
+    with pytest.raises(Exception):
         controller.get_user_by_email("user@example.com")
 
